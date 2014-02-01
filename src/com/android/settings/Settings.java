0408b4c8af2ad0f3ee445dc4b25aca4c1c5499fd
TheMrcool212 <munirmohamadrani@gmail.com>
2014-02-01 21:30:38 +0800

    Remove the connection manager option if the device doesn't have telephony function
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -559,6 +559,11 @@ public class Settings extends PreferenceActivity
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                     target.remove(i);
                 }
+            } else if (id == R.id.telo_radio_settings) {
+                // Remove ConnectionManager if the device does not have Telephony.
+                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+                    target.remove(i);
+                }
             } else if (id == R.id.data_usage_settings) {
                 // Remove data usage when kernel module not enabled
                 final INetworkManagementService netManager = INetworkManagementService.Stub
