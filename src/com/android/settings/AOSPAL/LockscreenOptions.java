df83a1448a51e5c201e3287d407db1eeac482d4b
tilaksidduram <tilaksidduram@gmail.com>
2014-03-26 02:44:26 +0530

    Revert custom lockscreen wallpaper [2/2]

--- src/com/android/settings/AOSPAL/LockscreenOptions.java
+++ src/com/android/settings/AOSPAL/LockscreenOptions.java
@@ -81,33 +81,9 @@ public class LockscreenOptions extends SettingsPreferenceFragment implements
 
     private static final String TAG = "LockscreenOptions";
 
-    private static final int REQUEST_CODE_BG_WALLPAPER = 1024;
-
-    private static final String KEY_LOCKSCREEN_WALLPAPER = "lockscreen_wallpaper";
-    private static final String KEY_SELECT_LOCKSCREEN_WALLPAPER = "select_lockscreen_wallpaper";
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
 
-
-    private CheckBoxPreference mLockscreenWallpaper;
-    private Preference mSelectLockscreenWallpaper;
     private CheckBoxPreference mBatteryStatus;
-
-    private File mWallpaperTemporary;
-
-    private IKeyguardService mKeyguardService;
-
-    private final ServiceConnection mKeyguardConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            mKeyguardService = IKeyguardService.Stub.asInterface(service);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            mKeyguardService = null;
-        }
-
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -116,21 +92,6 @@ public class LockscreenOptions extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.lockscreenoptions);
         ContentResolver resolver = getContentResolver();
 	PreferenceScreen prefs = getPreferenceScreen();
-
-        Intent intent = new Intent();
-        intent.setClassName("com.android.keyguard", "com.android.keyguard.KeyguardService");
-        if (!mContext.bindServiceAsUser(intent, mKeyguardConnection,
-                Context.BIND_AUTO_CREATE, UserHandle.OWNER)) {
-            Log.e(TAG, "*** Keyguard: can't bind to keyguard");
-        }
-
-        // Custom Lockscreen wallpaper
-        mLockscreenWallpaper = (CheckBoxPreference) findPreference(KEY_LOCKSCREEN_WALLPAPER);
-        mLockscreenWallpaper.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.LOCKSCREEN_WALLPAPER, 0) == 1);
-
-        mSelectLockscreenWallpaper = findPreference(KEY_SELECT_LOCKSCREEN_WALLPAPER);
-        mSelectLockscreenWallpaper.setEnabled(mLockscreenWallpaper.isChecked());
-        mWallpaperTemporary = new File(getActivity().getCacheDir() + "/lockwallpaper.tmp");
 
         mBatteryStatus = (CheckBoxPreference) prefs.findPreference(KEY_ALWAYS_BATTERY_PREF);
 
@@ -148,77 +109,9 @@ public class LockscreenOptions extends SettingsPreferenceFragment implements
         }
     }
 
-    public void onActivityResult(int requestCode, int resultCode,
-            Intent imageReturnedIntent) {
-        if (requestCode == REQUEST_CODE_BG_WALLPAPER) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (mWallpaperTemporary.length() == 0 || !mWallpaperTemporary.exists()) {
-                    Toast.makeText(getActivity(),
-                            getResources().getString(R.string.shortcut_image_not_valid),
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                Bitmap bmp = BitmapFactory.decodeFile(mWallpaperTemporary.getAbsolutePath());
-                try {
-                    mKeyguardService.setWallpaper(bmp);
-                    Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_SEE_THROUGH, 0);
-                } catch (Exception ex) {
-                    Log.e(TAG, "Failed to set wallpaper: " + ex);
-                }
-            }
-        }
-	if (mWallpaperTemporary.exists()) mWallpaperTemporary.delete();
-    }
-
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-	ContentResolver cr = getActivity().getContentResolver();
-        if (preference == mLockscreenWallpaper) {
-            if (!mLockscreenWallpaper.isChecked()) setWallpaper(null);
-            else Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_WALLPAPER, 1);
-            mSelectLockscreenWallpaper.setEnabled(mLockscreenWallpaper.isChecked());
-        } else if (preference == mSelectLockscreenWallpaper) {
-            final Intent intent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            intent.setType("image/*");
-            intent.putExtra("crop", "true");
-            intent.putExtra("scale", true);
-            intent.putExtra("scaleUpIfNeeded", false);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
-
-            final Display display = getActivity().getWindowManager().getDefaultDisplay();
-
-            boolean isPortrait = getResources().getConfiguration().orientation ==
-                    Configuration.ORIENTATION_PORTRAIT;
-
-            Point size = new Point();
-            display.getSize(size);
-
-            intent.putExtra("aspectX", isPortrait ? size.x : size.y);
-            intent.putExtra("aspectY", isPortrait ? size.y : size.x);
-
-            try {
-                mWallpaperTemporary.createNewFile();
-                mWallpaperTemporary.setWritable(true, false);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mWallpaperTemporary));
-                getActivity().startActivityFromFragment(this, intent, REQUEST_CODE_BG_WALLPAPER);
-            } catch (IOException e) {
-                // Do nothing here
-            } catch (ActivityNotFoundException e) {
-                // Do nothing here
-            }
-        }else {
 	    return super.onPreferenceTreeClick(preferenceScreen, preference);
-        }
-        return true;
-    }
-
-    private void setWallpaper(Bitmap bmp) {
-        try {
-            mKeyguardService.setWallpaper(bmp);
-        } catch (RemoteException ex) {
-            Log.e(TAG, "Unable to set wallpaper!");
-        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
