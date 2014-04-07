356a78e59904a615406895b3fac511390eb237c7
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2014-04-07 12:00:18 +0900

    SoundSettings: Assigning a null to an unnecessary variable
    Change-Id: Ic8c3e6ff66cc888f6069e3bbef666ede3c56dc13

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -266,6 +266,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                     getPreferenceScreen().removePreference(pref);
                 }
             }
+            mRingtonePreference = null;
         }
 
         mRingtoneLookupRunnable = new Runnable() {
