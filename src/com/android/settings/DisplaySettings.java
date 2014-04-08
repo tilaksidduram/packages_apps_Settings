64c55b833d52b1a807b2d0726999631bd256f759
houst0nn <houstonn@gmail.com>
2014-04-08 11:11:46 -0400

    Battery LED: put a state summary (enable/disable)
    Change-Id: Iba7648d6c96ed193db2296ee28d18fa4f00e0a30
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -333,6 +333,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
     }
 
+    private void updateBatteryPulseSummary() {
+        if (mBatteryPulse != null) {
+            if (Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.BATTERY_LIGHT_ENABLED, 1) == 1) {
+                mBatteryPulse.setSummary(R.string.notification_light_enabled);
+            } else {
+                mBatteryPulse.setSummary(R.string.notification_light_disabled);
+            }
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -397,6 +408,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         readFontSizePreference(mFontSizePref);
         updateScreenSaverSummary();
         updateLightPulseSummary();
+        updateBatteryPulseSummary();
     }
 
     private void updateScreenSaverSummary() {
