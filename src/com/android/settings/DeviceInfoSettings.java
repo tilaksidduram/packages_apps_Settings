LineageOS/android_packages_apps_Settings

Fix translatability of modified dev countdown strings
Change-Id: I3bc1749e27068a63144fd4f3b059396344e7858d

src/com/android/settings/DeviceInfoSettings.java
@@ -245,7 +245,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                     if (mDevHitToast != null) {
                         mDevHitToast.cancel();
                     }
-                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on,
+                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on_cm,
                             Toast.LENGTH_LONG);
                     mDevHitToast.show();
                 } else if (mDevHitCountdown > 0
@@ -254,7 +254,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                         mDevHitToast.cancel();
                     }
                     mDevHitToast = Toast.makeText(getActivity(), getResources().getQuantityString(
-                            R.plurals.show_dev_countdown, mDevHitCountdown, mDevHitCountdown),
+                            R.plurals.show_dev_countdown_cm, mDevHitCountdown, mDevHitCountdown),
                             Toast.LENGTH_SHORT);
                     mDevHitToast.show();
                 }
@@ -262,7 +262,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                 if (mDevHitToast != null) {
                     mDevHitToast.cancel();
                 }
-                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already,
+                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already_cm,
                         Toast.LENGTH_LONG);
                 mDevHitToast.show();
             }

tilaksidduram/packages_apps_Settings

src/com/android/settings/DeviceInfoSettings.java
@@ -227,7 +227,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                     if (mDevHitToast != null) {
                         mDevHitToast.cancel();
                     }
-                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on,
+                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on_pa,
                             Toast.LENGTH_LONG);
                     mDevHitToast.show();
                 } else if (mDevHitCountdown > 0
@@ -236,7 +236,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                         mDevHitToast.cancel();
                     }
                     mDevHitToast = Toast.makeText(getActivity(), getResources().getQuantityString(
-                            R.plurals.show_dev_countdown, mDevHitCountdown, mDevHitCountdown),
+                            R.plurals.show_dev_countdown_pa, mDevHitCountdown, mDevHitCountdown),
                             Toast.LENGTH_SHORT);
                     mDevHitToast.show();
                 }
@@ -244,7 +244,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                 if (mDevHitToast != null) {
                     mDevHitToast.cancel();
                 }
-                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already,
+                mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already_pa,
                         Toast.LENGTH_LONG);
                 mDevHitToast.show();
             }