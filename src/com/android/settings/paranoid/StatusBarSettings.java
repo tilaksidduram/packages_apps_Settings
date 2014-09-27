1a5b4c17ab7cf844d724286eb2fc5050ead99c66
tilaksidduram <tilaksidduram@gmail.com>
2014-09-27 18:12:52 +0530

    Support showing carrier/provider logo on statusbar [2/2]

--- src/com/android/settings/paranoid/StatusBarSettings.java
+++ src/com/android/settings/paranoid/StatusBarSettings.java
@@ -64,12 +64,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private static final String NETWORK_TRAFFIC_UNIT = "network_traffic_unit";
     private static final String NETWORK_TRAFFIC_PERIOD = "network_traffic_period";
     private static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_activity";
+    private static final String TOGGLE_CARRIER_LOGO = "toggle_carrier_logo";
 
     private CheckBoxPreference mStatusBarCustomHeader;
     private CheckBoxPreference mForceExpanded;
     private CheckBoxPreference mStatusBarNotifCount;
     private CheckBoxPreference mStatusBarBrightnessControl;
     private CheckBoxPreference mStatusBarNetworkActivity;
+    private CheckBoxPreference mToggleCarrierLogo;
     private ListPreference mNetTrafficState;
     private ListPreference mNetTrafficUnit;
     private ListPreference mNetTrafficPeriod;
@@ -88,20 +90,20 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
         loadResources();
 
-        ContentResolver resolver = getContentResolver();
+        ContentResolver mCr = getContentResolver();
 	PreferenceScreen prefSet = getPreferenceScreen();
 
         mStatusBarCustomHeader = (CheckBoxPreference) findPreference(KEY_STATUS_BAR_CUSTOM_HEADER);
-        mStatusBarCustomHeader.setChecked(Settings.System.getInt(getContentResolver(),
+        mStatusBarCustomHeader.setChecked(Settings.System.getInt(mCr,
               Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1);
 	mStatusBarCustomHeader.setOnPreferenceChangeListener(this);
 
         mForceExpanded = (CheckBoxPreference) prefSet.findPreference(FORCE_EXPANDED_NOTIFICATIONS);
-        mForceExpanded.setChecked((Settings.System.getInt(getContentResolver(),
+        mForceExpanded.setChecked((Settings.System.getInt(mCr,
                 Settings.System.FORCE_EXPANDED_NOTIFICATIONS, 0) == 1));
 
         mStatusBarNotifCount = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_NOTIF_COUNT);
-        mStatusBarNotifCount.setChecked(Settings.System.getInt(resolver,
+        mStatusBarNotifCount.setChecked(Settings.System.getInt(mCr,
                 Settings.System.STATUS_BAR_NOTIF_COUNT, 0) == 1);
         mStatusBarNotifCount.setOnPreferenceChangeListener(this);
 
@@ -113,12 +115,19 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
         mStatusBarBrightnessControl =
             (CheckBoxPreference) findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
-        mStatusBarBrightnessControl.setChecked((Settings.System.getInt(getContentResolver(),
+        mStatusBarBrightnessControl.setChecked((Settings.System.getInt(mCr,
                             Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0) == 1));
         mStatusBarBrightnessControl.setOnPreferenceChangeListener(this);
 
+        mToggleCarrierLogo = (CheckBoxPreference) findPreference(TOGGLE_CARRIER_LOGO);
+        mToggleCarrierLogo.setChecked((Settings.System.getInt(mCr,
+                Settings.System.TOGGLE_CARRIER_LOGO, 0) == 1));
+            if (!DeviceUtils.deviceSupportsMobileData(getActivity())) {
+            prefSet.removePreference(mToggleCarrierLogo);
+        }
+
         mStatusBarNetworkActivity = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_NETWORK_ACTIVITY);
-        mStatusBarNetworkActivity.setChecked(Settings.System.getInt(resolver,
+        mStatusBarNetworkActivity.setChecked(Settings.System.getInt(mCr,
             Settings.System.STATUS_BAR_NETWORK_ACTIVITY, 0) == 1);
          mStatusBarNetworkActivity.setOnPreferenceChangeListener(this);
 
@@ -129,7 +138,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         // TrafficStats will return UNSUPPORTED if the device does not support it.
         if (TrafficStats.getTotalTxBytes() != TrafficStats.UNSUPPORTED &&
                 TrafficStats.getTotalRxBytes() != TrafficStats.UNSUPPORTED) {
-            mNetTrafficVal = Settings.System.getInt(resolver, Settings.System.NETWORK_TRAFFIC_STATE, 0);
+            mNetTrafficVal = Settings.System.getInt(mCr, Settings.System.NETWORK_TRAFFIC_STATE, 0);
             int intIndex = mNetTrafficVal & (MASK_UP + MASK_DOWN);
             intIndex = mNetTrafficState.findIndexOfValue(String.valueOf(intIndex));
             if (intIndex <= 0) {
@@ -218,6 +227,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 	if (preference == mForceExpanded) {
             boolean checked = ((CheckBoxPreference)preference).isChecked();
             Settings.System.putInt(cr, Settings.System.FORCE_EXPANDED_NOTIFICATIONS, checked ? 1:0);
+        } else if (preference == mToggleCarrierLogo) {
+            Settings.System.putInt(getContentResolver(), Settings.System.TOGGLE_CARRIER_LOGO, mToggleCarrierLogo.isChecked() ? 1 : 0);
+            return true;
         }else {
          return super.onPreferenceTreeClick(preferenceScreen, preference);
        }
