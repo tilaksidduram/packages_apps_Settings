7b12a00dc18ce35aa7da54c9b434b45885e9e1b7
DvTonder <david.vantonder@gmail.com>
2015-02-11 08:25:13 -0500

    Fix Profiles settings screen padding
    Change-Id: Iff56f777141cc1e56492da7a2b20e2af02a7db8f

--- src/com/android/settings/profiles/ProfilesSettings.java
+++ src/com/android/settings/profiles/ProfilesSettings.java
@@ -134,11 +134,6 @@ public class ProfilesSettings extends SettingsPreferenceFragment {
 
         // check if we are enabled
         updateProfilesEnabledState();
-
-        // If running on a phone, remove padding around tabs
-        if (!ScreenType.isTablet(getActivity())) {
-            mContainer.setPadding(0, 0, 0, 0);
-        }
     }
 
     @Override
