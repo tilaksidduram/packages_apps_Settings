7f8dc8db9c92831aa65442fd5d1106f37fa90755
DvTonder <david.vantonder@gmail.com>
2015-02-11 08:25:13 -0500

    Fix Profiles settings screen padding
    Change-Id: Iff56f777141cc1e56492da7a2b20e2af02a7db8f

--- src/com/android/settings/profiles/ProfilesList.java
+++ src/com/android/settings/profiles/ProfilesList.java
@@ -49,11 +49,6 @@ public class ProfilesList extends SettingsPreferenceFragment implements
     public void onResume() {
         super.onResume();
         refreshList();
-
-        // On tablet devices remove the padding
-        if (ScreenType.isTablet(getActivity())) {
-            getListView().setPadding(0, 0, 0, 0);
-        }
     }
 
     public void refreshList() {
