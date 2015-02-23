8e3de260e60b90762c95ee9087f09f332a69cd65
tilaksidduram <tilaksidduram@gmail.com>
2015-02-23 21:35:53 +0530

    QS: add Screenshot (2/2)
    Credits to Altaf-Mahdi

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -134,6 +134,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_brightness_auto_off";
                 stringId = R.string.qs_brightness_tile;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_screenshot_tile;
+                break;
             default:
                 return null;
         }
