b1f12a41f4eb4acc450a6c3170192e8e4b5b39ff
Alex Cruz <mazdarider23@gmail.com>
2015-02-24 14:35:11 +0200

    App Circle Bar & NavBar tile [2/2]
    Change-Id: I1e057c66a81766d6bb50065bb597d198638e9d3b
    
    Conflicts:
    	res/values/pa_strings.xml

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -166,6 +166,14 @@ public class QSTileHolder {
                 resourceName = "ic_qs_reboot";
                 stringId = R.string.qs_tile_reboot;
                 break;
+            case QSConstants.TILE_NAVBAR:
+                resourceName = "ic_qs_navbar_on";
+                stringId = R.string.qs_navbar_tile;
+                break;
+            case QSConstants.TILE_APPCIRCLEBAR:
+                resourceName = "ic_qs_appcirclebar_on";
+                stringId = R.string.qs_appcirclebar_tile;
+                break;
             default:
                 return null;
         }
