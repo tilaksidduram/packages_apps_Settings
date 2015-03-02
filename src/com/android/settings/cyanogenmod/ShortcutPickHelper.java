4a475d9c3372bd974438f5f5d23e59e70f2ba575
tilaksidduram <tilaksidduram@gmail.com>
2015-03-02 23:17:29 +0530

    derp on shortcutpickerhelper

--- src/com/android/settings/cyanogenmod/ShortcutPickHelper.java
+++ src/com/android/settings/cyanogenmod/ShortcutPickHelper.java
@@ -77,11 +77,8 @@ public class ShortcutPickHelper {
                 processShortcut(data, REQUEST_PICK_APPLICATION, REQUEST_CREATE_SHORTCUT);
                 break;
             }
-<<<<<<< HEAD
         } else {
             mListener.shortcutPicked(null, null, false);
-=======
->>>>>>> 1f30a9c... Settings: Gesture Anywhere - bring it back [2/2]
         }
     }
 
@@ -258,17 +255,10 @@ public class ShortcutPickHelper {
                 ViewGroup parent) {
             if (convertView == null) {
                 convertView = View.inflate(mParent, android.R.layout.simple_list_item_1, null);
-<<<<<<< HEAD
                 convertView.setPadding(groupPadding, 0, 0, 0);
             }
             TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
             textView.setText(getGroup(groupPosition).label);
-=======
-                convertView.setPadding(70, 0, 0, 0);
-            }
-            TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
-            textView.setText(getGroup(groupPosition).label.toString());
->>>>>>> 1f30a9c... Settings: Gesture Anywhere - bring it back [2/2]
             return convertView;
         }
 
