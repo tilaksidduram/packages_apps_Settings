a40360d216ccc9665c0c3cc7e7354f021e1b76f2
tilaksidduram <tilaksidduram@gmail.com>
2015-03-11 09:31:33 +0530

    remove all references of org.cyanogenmod.hardware

--- Android.mk
+++ Android.mk
@@ -36,8 +36,6 @@ LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.v7.cardview
 
-LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
-
 include frameworks/opt/setupwizard/navigationbar/common.mk
 include $(BUILD_PACKAGE)
 
