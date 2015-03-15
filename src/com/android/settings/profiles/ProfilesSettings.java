99843401936395e720b74743fc6843a808c0cefd
tilaksidduram <tilaksidduram@gmail.com>
2015-03-15 14:28:11 +0530

    somes fixes to profile settings

--- src/com/android/settings/profiles/ProfilesSettings.java
+++ src/com/android/settings/profiles/ProfilesSettings.java
@@ -61,8 +61,7 @@ import com.android.settings.cyanogenmod.BaseSystemSettingSwitchBar;
 import java.util.UUID;
 
 public class ProfilesSettings extends SettingsPreferenceFragment
-        implements BaseSystemSettingSwitchBar.SwitchBarChangeCallback,
-        Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "ProfilesSettings";
 
     public static final String EXTRA_PROFILE = "Profile";
