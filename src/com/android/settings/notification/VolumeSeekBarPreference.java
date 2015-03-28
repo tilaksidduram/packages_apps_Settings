0eed9aae4be1ef99d27a11b113263f85cbf2a7ff
tilaksidduram <tilaksidduram@gmail.com>
2015-03-28 15:16:27 +0530

    Revert "Add back increasing ring feature (2/3)."
    This reverts commit e84ac76bfad98d0e4996a05f8bd6eb550aafc4de.
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/notification_settings.xml
    	src/com/android/settings/notification/IncreasingRingVolumePreference.java
    	src/com/android/settings/notification/NotificationSettings.java

--- src/com/android/settings/notification/VolumeSeekBarPreference.java
+++ src/com/android/settings/notification/VolumeSeekBarPreference.java
@@ -89,7 +89,6 @@ public class VolumeSeekBarPreference extends SeekBarPreference
         mStopped = true;
         if (mVolumizer != null) {
             mVolumizer.stop();
-            mVolumizer = null;
         }
     }
 
