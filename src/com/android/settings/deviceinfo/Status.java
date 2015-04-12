e4691ff74b83bd56e3db34d98a90891d741091b2
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-04-12 17:22:10 +0000

    Settings: use CMHW serial number
    See http://review.cyanogenmod.org/57164
    
    Change-Id: If328ca7b487435b88246d1f8c47b04770183f09e

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -59,6 +59,8 @@ import com.android.settings.R;
 import com.android.settings.SelectSubscription;
 import com.android.settings.Utils;
 
+import org.cyanogenmod.hardware.SerialNumber;
+
 import java.lang.ref.WeakReference;
 
 /**
@@ -379,7 +381,7 @@ public class Status extends PreferenceActivity {
 
         updateConnectivity();
 
-        String serial = Build.SERIAL;
+        String serial = getSerialNumber();
         if (serial != null && !serial.equals("")) {
             setSummaryText(KEY_SERIAL_NUMBER, serial);
         } else {
@@ -673,5 +675,17 @@ public class Status extends PreferenceActivity {
 
     private boolean isMultiSimEnabled() {
         return (TelephonyManager.getDefault().getPhoneCount() > 1);
+    }
+
+    private String getSerialNumber() {
+        try {
+            if (SerialNumber.isSupported()) {
+                return SerialNumber.getSerialNumber();
+            }
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed; fall through
+        }
+
+        return Build.SERIAL;
     }
 }
