cc31b8828cd950715b7e41f1b8857de85ade3d4a
Valter Strods <valterstrods@aospa.co>
2015-08-13 15:46:52 +0300

    OnTheSpot: Update the state after resetting
    If all the settings get reset, we want to disable the button for the
    bestest UX. This commit adds just that to the flow.
    
    Change-Id: Ifa8f2caee7384270c2e79a3ea2999546835ae083

--- src/com/android/settings/PrivacySettings.java
+++ src/com/android/settings/PrivacySettings.java
@@ -263,6 +263,7 @@ public class PrivacySettings extends SettingsPreferenceFragment implements
                     }
                 }
 
+                updateResetUserPreferences();
                 dialog.dismiss();
             }
 
