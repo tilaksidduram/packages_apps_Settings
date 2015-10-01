38bb910af35b304ab89deba680b38ce511205da8
tilaksidduram <tilaksidduram@gmail.com>
2015-10-01 20:00:06 +0530

    Settings: Add Pie Tile

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -208,6 +208,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_themes";
                 stringId = R.string.qs_tile_themes;
                 break;
+            case QSConstants.TILE_PIE:
+                resourceName = "ic_qs_pie_on";
+                stringId = R.string.qs_tile_pie;
+                break;
 
             // Dynamic tiles
             case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
