f8e7da5fab34518ece928a3f996689923e63ef85
tilaksidduram <tilaksidduram@gmail.com>
2016-07-15 09:50:12 +0530

    settings: add default color value in QS Shade Stroke

--- src/com/android/settings/slim/SoundSettings.java
+++ src/com/android/settings/slim/SoundSettings.java
@@ -21,7 +21,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
-import android.content.ContentResolver
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.UserHandle;
